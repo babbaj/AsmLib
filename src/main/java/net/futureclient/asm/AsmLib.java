dc2242446f6426337f1a6785376740ffd35da0ca
0x22 <9968651+0-x-2-2@users.noreply.github.com>
2018-05-11 23:07:01 -0400

    Initial commit

---
+++ src/main/java/net/futureclient/asm/AsmLib.java
@@ -0,0 +1,11 @@
+package net.futureclient.asm;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class AsmLib {
+
+    private static final Logger logger = LogManager.getLogger("asmlib");
+
+    private AsmLib() {}
+}
