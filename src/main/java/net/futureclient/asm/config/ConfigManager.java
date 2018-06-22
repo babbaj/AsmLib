c627f609235410c5cf3c6a28cd6a3267201fed15
babbaj <babbaj45@gmail.com>
2018-06-22 02:18:32 -0400

    don't think that actually works like i thought

--- src/main/java/net/futureclient/asm/config/ConfigManager.java
+++ src/main/java/net/futureclient/asm/config/ConfigManager.java
@@ -9,7 +9,7 @@ public class ConfigManager {
 
     private final Config default_config = new Config("default", 0);
 
-    private final Multiset<Config> configs = TreeMultiset.create(Config::compareTo);
+    private final List<Config> configs = new ArrayList<>();
     { configs.add(default_config); }
 
     public static final ConfigManager INSTANCE = new ConfigManager();
@@ -18,6 +18,7 @@ public class ConfigManager {
 
     public void addConfiguration(Config config) {
         this.configs.add(config);
+        configs.sort(Config::compareTo);
     }
 
     public Collection<Config> getConfigs() {
